22bdd919cb3a3e67f325fe1ea2a8698193a189f8
Lucas Samel <lucas.samel97@gmail.com>
2021-02-23 21:37:38 -0300

    Teste FirebaseFirestore realizado com sucesso

--- app/src/main/java/br/com/greenlight/MainActivity.kt
+++ app/src/main/java/br/com/greenlight/MainActivity.kt
@@ -2,21 +2,26 @@ package br.com.greenlight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.annotation.NonNull
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import br.com.greenlight.R
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var drawerLayout: DrawerLayout
     lateinit var navController: NavController
     lateinit var navView: NavigationView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,10 +34,30 @@ class MainActivity : AppCompatActivity() {
         //Implementa o menu hamburguer
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         NavigationUI.setupWithNavController(navView, navController)
+
+        //Teste FirebaseFirestore
+//        val firebaseFirestore = FirebaseFirestore.getInstance()
+//        val collection = firebaseFirestore.collection("carros")
+//
+//        val produto = mapOf(
+//            "modelo" to "Troller",
+//            "marca" to "Troller",
+//            "combustivel" to "disel"
+//        )
+//
+//        val task = collection.add(produto)
+//        task.addOnCompleteListener {
+//            if (it.isSuccessful)
+//                Log.i("Firestore", "Cadastro com sucesso")
+//            else
+//                Log.i("Firestore","Falha ao cadastrar")
+//        }
     }
 
     //Método para processar a navegação, inclusive o clique da navegação
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(navController, drawerLayout)
     }
+
+
 }
