75e0d57944ae496b220471631212c169be47bf6a
Philippo <philippo.rodrigues@mobicare.com.br>
2021-03-15 22:14:43 -0300

    Voltando a seleção do usuário pelo uid

--- app/src/main/java/br/com/greenlight/model/User.kt
+++ app/src/main/java/br/com/greenlight/model/User.kt
@@ -3,11 +3,11 @@ package br.com.greenlight.model
 import com.google.firebase.firestore.DocumentId
 
 class User(
-    @DocumentId
     var nome: String? = null,
     var username: String? = null,
     var endereco: String? = null,
     var codigoPostal: String? = null,
     var dataNascimento: String? = null,
+    @DocumentId
     val uid: String? = null
 )
