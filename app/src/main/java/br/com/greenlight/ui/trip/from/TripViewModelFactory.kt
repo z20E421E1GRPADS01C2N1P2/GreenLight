2116d69248664b625071129dc960cf2efaf668d3
Isaac Lobo <lobo.isaac92@gmail.com>
2021-03-15 22:06:38 -0300

    Atualização de api de distancia

--- app/src/main/java/br/com/greenlight/ui/trip/from/TripViewModelFactory.kt
+++ app/src/main/java/br/com/greenlight/ui/trip/from/TripViewModelFactory.kt
@@ -6,10 +6,13 @@ import androidx.lifecycle.ViewModelProvider
 import br.com.greenlight.database.dao.TripDao
 
 class TripViewModelFactory (private val tripDao: TripDao,
-                            private val application: Application): ViewModelProvider.Factory {
+                            private val application: Application,
+                            private val destino: String?, private val
+                            origem:String?):
+    ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(TripViewModel::class.java))
-            return TripViewModel(tripDao, application) as T
+            return TripViewModel(tripDao, application,destino,origem) as T
         throw IllegalArgumentException("Classe ViewModel desconhecida.")
     }
 }
