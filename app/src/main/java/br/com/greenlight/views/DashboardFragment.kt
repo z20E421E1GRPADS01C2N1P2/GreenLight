5bc874f1bd7a76741c62d1a42ce125acb2428eeb
Philippo <philippo.rodrigues@mobicare.com.br>
2021-04-02 17:05:09 -0300

    Implementando String no strings.xml

--- app/src/main/java/br/com/greenlight/views/DashboardFragment.kt
+++ app/src/main/java/br/com/greenlight/views/DashboardFragment.kt
@@ -3,17 +3,11 @@ package br.com.greenlight.views
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import androidx.navigation.fragment.findNavController
 import br.com.greenlight.R
-import com.google.android.gms.ads.AdListener
 import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.MobileAds
-import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 class DashboardFragment : Fragment() {
@@ -27,6 +21,7 @@ class DashboardFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         val adRequest = AdRequest.Builder().build()
         adView.loadAd(adRequest)
         btnMapa.setOnClickListener {
