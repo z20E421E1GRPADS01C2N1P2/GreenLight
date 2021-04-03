93b331ef7acb593c14817fb22311560c099a2b7e
Lucas Samel <lucas.samel97@gmail.com>
2021-04-02 23:47:02 -0300

    Layout List Vehicle Recycler

--- app/src/main/java/br/com/greenlight/ui/vehicle/form/VehicleFragment.kt
+++ app/src/main/java/br/com/greenlight/ui/vehicle/form/VehicleFragment.kt
@@ -7,9 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.*
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import br.com.greenlight.R
@@ -18,7 +17,7 @@ import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.vehicle_fragment.*
 
 
-class VehicleFragment() : Fragment() {
+class VehicleFragment : Fragment() {
 
     private lateinit var viewModel: VehicleViewModel
 
@@ -36,7 +35,7 @@ class VehicleFragment() : Fragment() {
         viewModel = ViewModelProvider(this, formCarroViewModelFactory)
             .get(VehicleViewModel::class.java)
 
-        viewModel.status.observe(viewLifecycleOwner, Observer { status ->
+        viewModel.status.observe(viewLifecycleOwner, { status ->
             if (status)
                 findNavController().popBackStack()
         })
